ecc1bff0789fc941916aef5634e1b922e742c042
Patrick Chan <q_p18@hotmail.com>
2020-12-10 17:27:11 +0000

    Complete palindromes

--- Fundamentals 5/palindromes/palindromes.js
+++ Fundamentals 5/palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function() {
+/* Resources
+Repl to test code https://repl.it/@pchan01/palindromes#index.js
+Reverse a string https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+Regex https://regex101.com/
+*/
 
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
+}
+  
+const palindromes = function(str) {
+    let lowerCaseStr = str.toLowerCase();
+    let strWithoutNonWordCharacter = lowerCaseStr.replace(/\W/g, "");  
+    let reversedStr = reverseString(strWithoutNonWordCharacter);
+  
+    // Alternative -> if(strWithoutNonWordCharacter === reversedStr)
+    if(strWithoutNonWordCharacter.match(reversedStr)) {
+      return true;
+    } return false;
 }
 
 module.exports = palindromes
