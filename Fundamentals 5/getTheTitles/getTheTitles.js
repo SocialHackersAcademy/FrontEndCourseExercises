924538c4846ca7f68aaabdbe46c5ba7abff5c833
Patrick Chan <q_p18@hotmail.com>
2020-12-10 16:50:07 +0000

    Complete getTheTitles

--- Fundamentals 5/getTheTitles/getTheTitles.js
+++ Fundamentals 5/getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArr) {
+    let titles = booksArr.map(book => book.title);
+    return titles;
 }
 
 module.exports = getTheTitles;
