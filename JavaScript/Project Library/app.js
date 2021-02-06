b428a5933a0af773d4106d3d916c3587b3590483
Patrick Chan <q_p18@hotmail.com>
2021-02-06 20:26:41 +0000

    Add randomColor variable. Create render function to display one book

--- JavaScript/Project Library/app.js
+++ JavaScript/Project Library/app.js
@@ -21,10 +21,63 @@ addBookToLibrary(theElephantTree);
 addBookToLibrary(requiemForADream);
 addBookToLibrary(trainspotting);
 
-const bookShelf = document.getElementById('book-shelf');
+const bookcase = document.getElementById('bookcase');
 const form = document.getElementById('form');
 const formButton = document.getElementById('form-button');
 const submitButton = document.getElementById('submit-button');
+const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+const render = () => {
+
+     // myLibrary.forEach(book => {
+     //      book = document.createElement('div');
+     //      bookcase.appendChild(book);
+     //      book.style.display = 'inline-block';
+     //      book.style.width = '2.5rem';
+     //      book.style.height = '4rem';
+     //      book.style.background = `#${randomColor}`;
+     // })
+
+     // const myLibraryLength = myLibrary.length;
+     // for (let i = 0; i < myLibraryLength; i++) {
+     //      myLibrary[i] = document.createElement('div');
+     //      bookcase.appendChild(myLibrary[i]);
+     //      myLibrary[i].style.display = 'inline-block';
+     //      myLibrary[i].style.width = '2.5rem';
+     //      myLibrary[i].style.height = '4rem';
+     //      myLibrary[i].style.background = `#${randomColor}`;
+     // }
+
+     const bookContainer = document.createElement('article');
+     bookcase.appendChild(bookContainer);
+     bookcase.style.margin = '.5rem 1rem';
+
+     const book = document.createElement('div');
+     bookContainer.appendChild(book);
+     book.setAttribute('class', 'book');
+     book.style.background = `#${randomColor}`;
+
+     const title = document.createElement('p');
+     book.appendChild(title);
+     title.textContent = myLibrary[0].title;
+
+     const author = document.createElement('p');
+     book.appendChild(author);
+     author.setAttribute('class', 'author');
+     author.textContent = `by ${myLibrary[0].author}`;
+
+     const removeBtn = document.createElement('button');
+     bookContainer.appendChild(removeBtn);
+     removeBtn.setAttribute('class', 'remove-btn')
+     removeBtn.textContent = "Remove";
+
+     const statusBtn = document.createElement('button');
+     bookContainer.appendChild(statusBtn);
+     statusBtn.setAttribute('class', 'status-btn')
+     statusBtn.textContent = "Unread";
+}
+
+render();
 
 let state = false;
 
